64d7ff7937b2fd814242ecd67378cc83ca1e4e7c
CygnusMoci <nieyinghao@megvii.com>
2020-03-13 17:44:57 +0800

    add - 新增yuv420p相关转换方法 add - 新增vivo等机型的测试兼容

--- app/src/main/java/com/moci/defaultandroid/util/mImg.java
+++ app/src/main/java/com/moci/defaultandroid/util/mImg.java
@@ -20,6 +20,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  * @author : Cygnusmoci
@@ -190,6 +191,90 @@ public class mImg {
         byte[] imgBytes = baos.toByteArray();
         return imgBytes;
     }
+
+
+    /**
+     * RGB的bitmap转RGBA
+     * @param image
+     * @return
+     */
+    public static byte[] getPixelsRGBA(Bitmap image) {
+        // calculate how many bytes our image consists of
+        int bytes = image.getByteCount();
+        ByteBuffer buffer = ByteBuffer.allocate(bytes); // Create a new buffer
+        image.copyPixelsToBuffer(buffer); // Move the byte data to the buffer
+        byte[] temp = buffer.array(); // Get the underlying array containing the data.
+        byte[] pixels = new byte[temp.length]; // Allocate for RGBA
+        // Copy pixels into place
+        for (int i = 0; i < (temp.length / 4); i++) {
+            pixels[i * 4 + 0] = temp[i * 4 + 0];       //R
+            pixels[i * 4 + 1] = temp[i * 4 + 1];       //G
+            pixels[i * 4 + 2] = temp[i * 4 + 2];       //B
+            pixels[i * 4 + 3] = temp[i * 4 + 3];       //A
+        }
+        return pixels;
+    }
+
+    /**
+     * RGB的bitmap图转YUV21
+     * @param bitmap
+     * @return
+     */
+    public static byte[] convertYUV21FromRGB(Bitmap bitmap){
+        bitmap = rotaingImageView(90, bitmap);
+
+        int inputWidth = bitmap.getWidth();
+        int inputHeight = bitmap.getHeight();
+
+        int[] argb = new int[inputWidth * inputHeight];
+
+        bitmap.getPixels(argb, 0, inputWidth, 0, 0, inputWidth, inputHeight);
+
+        byte[] yuv = new byte[inputWidth * inputHeight * 3 / 2];
+
+        encodeYUV420SP(yuv, argb, inputWidth, inputHeight);
+
+        bitmap.recycle();
+
+        return yuv;
+
+    }
+
+    private static void encodeYUV420SP(byte[] yuv420sp, int[] argb, int width, int height) {
+        final int frameSize = width * height;
+
+        int yIndex = 0;
+        int uvIndex = frameSize;
+
+        int a, R, G, B, Y, U, V;
+        int index = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+
+                a = (argb[index] & 0xff000000) >> 24; // a is not used obviously
+                R = (argb[index] & 0xff0000) >> 16;
+                G = (argb[index] & 0xff00) >> 8;
+                B = (argb[index] & 0xff) >> 0;
+
+                // well known RGB to YUV algorithm
+                Y = ((66 * R + 129 * G + 25 * B + 128) >> 8) + 16;
+                U = ((-38 * R - 74 * G + 112 * B + 128) >> 8) + 128;
+                V = ((112 * R - 94 * G - 18 * B + 128) >> 8) + 128;
+
+                // NV21 has a plane of Y and interleaved planes of VU each sampled by a factor of 2
+                //    meaning for every 4 Y pixels there are 1 V and 1 U.  Note the sampling is every other
+                //    pixel AND every other scanline.
+                yuv420sp[yIndex++] = (byte) ((Y < 0) ? 0 : ((Y > 255) ? 255 : Y));
+                if (j % 2 == 0 && index % 2 == 0) {
+                    yuv420sp[uvIndex++] = (byte) ((V < 0) ? 0 : ((V > 255) ? 255 : V));
+                    yuv420sp[uvIndex++] = (byte) ((U < 0) ? 0 : ((U > 255) ? 255 : U));
+                }
+
+                index++;
+            }
+        }
+    }
+
 
     public static void write(byte[] mImage, File mImageFile){
         try {
